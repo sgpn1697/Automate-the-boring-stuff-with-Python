9020c3b4113b328bbb76bbf5177067f5a65a22dc
sgpn1697 <56813959+sgpn1697@users.noreply.github.com>
2020-06-23 04:36:08 +0000

    Update and rename Code1 to Project 1.py

---
+++ Project 1.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+print ('What are you looking for mortal')
+print ('google1,google2,micr, ies, covcalifornia,tryhackme, ch, nor, SGovi67time, sagovi0096, simplenote,0005742685DHS, ' )
+name = input()
+if name == 'google1':
+	print ('goo16')
+if name == 'google2':
+	print('goo162')
+if name == 'micr':
+	print('mic162')
+if name == 'ies':
+	print ('ies')
+if name == 'covcalifornia':
+	print ('cov')
+	print ('savangov')
+if name == 'tryhackme':
+	print('try')
+if name == 'ch':
+	print('cha16')
+if name == 'nor':
+	print('nord16')
+if name == 'SGovi67':
+	print ('ies')
+
+if name == 'PIHole':
+	print('pihole')
+if name == 'Simplenote':
+	print('sim16')
+if name == '0005742685':
+	print('normal')
+
+
+
+
+
